8d4867bc212006c24dce213f7027e628acb4a165
Diavva <Karmasin@interia.pl>
2022-01-13 16:53:45 +0100

    Almost, need to make it so it's not just a jumble of letters

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
@@ -13,16 +14,48 @@ public class Main {
             b) stworzyć 3 objekty klasy student i wstawić je do tablicy
             c) wyświetlić dane z tablicy, użyć pętli for lub forEach
             d) dane powinny być estetycznie przedstawione
-                String Imię;
-    String Nazwisko;
-    int rok;
-    boolean attending;     */
+    */
 
-        List StudentList = new ArrayList();
 
-        Student s1 = new Student();
-        Student s2 = new Student();
-        Student s3 = new Student();
+
+
+
+        Student student1 = new Student(){
+
+        String Imię = "Basia";
+        String Nazwisko = "Kowalska";
+        int rok = 3;
+        boolean attending = true;
+        };
+
+
+        Student student2 = new Student() {
+
+            String Imię = "Kasia";
+            String Nazwisko = "Kowalska";
+            int rok = 5;
+            boolean attending = false;
+        };
+
+        Student student3 = new Student() {
+
+            String Imię = "Daniel";
+            String Nazwisko = "Stachowski";
+            int rok = 8;
+            boolean attending = true;
+        };
+
+        ArrayList<Student> studentci = new ArrayList<>();
+        studentci.add(student1);
+        studentci.add(student2);
+        studentci.add(student3);
+
+        for(Student student: studentci);
+        {
+            System.out.println(student1  + "\n" + student2 + "\n" + student3);
+        }
+
+
 
 
 
